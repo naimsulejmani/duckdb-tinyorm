88b90803fdb68f39416c0179d2c708848214af7b
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-09-29 19:53:52 +0200

    Changed the structure of export and folders

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import 'reflect-metadata';
-import { DataTypeDecorator, Id, Unique } from "./constants/data-type.decorator";
-import { BaseRepository, Entity, Repository } from "./repositories/base.repository";
+import { DataTypeDecorator, Entity, Id, Repository, Unique } from "./constants/data-type.decorator";
+import { BaseRepository } from "./repositories/base.repository";
 import { DuckDbRepository } from "./repositories/duckdb.repository";
 import { IRepository } from './repositories/base.interface';
 
 
 
-module.exports = {
-    DataTypeDecorator, Id, Unique, BaseRepository, Entity, Repository, DuckDbRepository, IRepository
-}
+export {
+    DataTypeDecorator,
+    Id,
+    Unique,
+    BaseRepository,
+    Entity,
+    Repository,
+    DuckDbRepository,
+    IRepository
+};
