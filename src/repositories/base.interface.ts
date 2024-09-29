c01b0b9cd1eaca6164dd8bf3545b79b0e53c9ab7
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-09-29 19:31:00 +0200

    Added interface for base repository

---
+++ src/repositories/base.interface.ts
@@ -0,0 +1,6 @@
+export interface IRepository<T, Tid> {
+    save(entity: T): Promise<T>;
+    findAll(): Promise<T[]>;
+    findById(id: Tid): Promise<T>
+    findBy(entity: T, columns: string[])
+}
